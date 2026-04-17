886cb8e8a93831c630a3d5fb8737aee9153ebca7
Diniz Martins <rodr0304@algonquinlive.com>
2026-04-16 22:13:39 -0400

    Update query.sql

--- stream-analytics/query.sql
+++ stream-analytics/query.sql
@@ -1,11 +1,39 @@
+WITH AggregatedData AS
+(
+    SELECT
+        location,
+        AVG(iceThickness) AS avgIceThickness,
+        MIN(iceThickness) AS minIceThickness,
+        MAX(iceThickness) AS maxIceThickness,
+        AVG(surfaceTemp) AS avgSurfaceTemp,
+        MIN(surfaceTemp) AS minSurfaceTemp,
+        MAX(surfaceTemp) AS maxSurfaceTemp,
+        MAX(snowAccumulation) AS maxSnowAccumulation,
+        AVG(externalTemp) AS avgExternalTemp,
+        COUNT(*) AS readingCount,
+        System.Timestamp() AS processedTime
+    FROM [iot-input]
+    GROUP BY
+        location,
+        TumblingWindow(minute, 5)
+)
+
 SELECT
     location,
-    iceThickness,
-    surfaceTemp,
-    snowAccumulation,
-    externalTemp,
-    System.Timestamp AS processedTime
-INTO
-    output
-FROM
-    [iot-input]
+    avgIceThickness,
+    minIceThickness,
+    maxIceThickness,
+    avgSurfaceTemp,
+    minSurfaceTemp,
+    maxSurfaceTemp,
+    maxSnowAccumulation,
+    avgExternalTemp,
+    readingCount,
+    processedTime,
+    CASE
+        WHEN avgIceThickness >= 30 AND avgSurfaceTemp <= -2 THEN 'Safe'
+        WHEN avgIceThickness >= 25 AND avgSurfaceTemp <= 0 THEN 'Caution'
+        ELSE 'Unsafe'
+    END AS safetyStatus
+INTO output
+FROM AggregatedData
